0a16d03a62ede6b4df35bd8a4bc86a1ce44e191e
Hoa Nguyen <hoa.nguyen@pharmapoc.com>
2022-06-08 15:39:38 +0700

    sample hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,5 +2,22 @@
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.7.3",
+  solidity: "0.8.4",
+  networks: {
+    testnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      chainId: 97,
+      gasPrice: 20000000000,
+      accounts: {mnemonic: mnemonic} // process.env.PRIVATE_KEY_TESTNET
+    },
+    mainnet: {
+      url: "https://bsc-dataseed.binance.org/",
+      chainId: 56,
+      gasPrice: 20000000000,
+      accounts: {mnemonic: mnemonic} // process.env.PRIVATE_KEY_MAINNET
+    }
+  },
+  // etherscan: {
+  //   apiKey: process.env.apiKey
+  // }
 };
