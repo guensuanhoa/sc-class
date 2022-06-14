cd14a716b961dc28c968702c728252718ff7bd13
hoa.nguyen <hoa.nguyen@pharmapoc.com>
2022-06-15 05:25:16 +0700

    test contract gold

--- test/test-gold.js
+++ test/test-gold.js
@@ -49,12 +49,51 @@ describe("Gold", function () {
     })
 
     describe("unpause", function () {
-        it("revert if has been not paused", async function () {})
-        it("revert if not pauser role", async function () {})
-        it("pause success", async function () {})
+        beforeEach(async () => {
+            await token.pause()
+        })
+        it("revert if has been unpaused", async function () {
+            await token.unpause()
+            await expect(token.unpause()).to.be.revertedWith("Pausable: not paused")
+        })
+        it("revert if not pauser role", async function () {
+            await expect(token.connect(accountB).unpause()).to.be.reverted
+        })
+        it("unpause success", async function () {
+            await expect(await token.unpause()).to.be.emit(token, "Unpaused").withArgs(accountA.address)
+            await expect(await token.transfer(accountB.address, amount)).to.be.emit(token, "Transfer").withArgs(accountA.address, accountB.address, amount)
+        })
     })
 
-    describe("addToBlackList", function () {})
+    describe("addToBlackList", function () {
+        it("revert if sender is not admin", async function() {
+            await expect(token.connect(accountB.address).addToBlackList(accountC.address)).to.be.reverted
+        })
+        it("revert if add sender to blacklist", async function() {
+            await expect(token.addToBlackList(accountA.address)).to.be.revertedWith("Gold: must not add sender to blacklist")
+        })
+        it("revert if account was in backlist", async function() {
+            await token.addToBlackList(accountB.address)
+            await expect(token.addToBlackList(accountB.address)).to.be.revertedWith("Gold: account was on blacklist")
+        })
+        it("addToBlackList success", async function () {
+            expect(await token.addToBlackList(accountB.address)).to.be.emit(token, "BlackListAdded").withArgs(accountB.address)
+            await expect(token.transfer(accountB.address, amount)).to.be.revertedWith("Gold: account recipient was on blacklist")
+            await expect(token.connect(accountB).transfer(accountC.address, amount)).to.be.revertedWith("Gold: account sender was on blacklist")
+        })
+    })
 
-    describe("removeFromBlackList", function () {})
+    describe("removeFromBlackList", function () {
+        it("revert if sender is not addmin", async function() {
+            await expect(token.connect(accountB.address).removeFromBlackList(accountC.address)).to.be.reverted
+        })
+        it("revert if account was not in blacklist", async function() {
+            await expect(token.removeFromBlackList(accountB.address)).to.be.revertedWith("Gold: account was not on blacklist")
+        })
+        it("remove success", async function() {
+            await token.addToBlackList(accountB.address)
+            expect(await token.removeFromBlackList(accountB.address)).to.be.emit(token, "BlackListRemoved").withArgs(accountB.address)
+            await expect(token.transfer(accountB.address, amount)).to.be.emit(token, "Transfer").withArgs(accountA.address, accountB.address, amount)
+        })
+    })
 })
