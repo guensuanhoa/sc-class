00b81556bbf881006a2d561136e6bf276d0fbd97
hoa.nguyen <hoa.nguyen@pharmapoc.com>
2022-06-21 05:25:38 +0700

    create contract petty

---
+++ test/nft/petty.js
@@ -0,0 +1,9 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Petty", function () {
+    let [accountA, accountB, accountC] = []
+    let petty
+    let address0 = "0x0000000000000000000000000000000000000000"
+    let uri = "sampleurl.com/"
+})
